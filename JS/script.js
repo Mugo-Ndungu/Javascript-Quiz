5b3eafa15baf611eb4f7c064918111f303a7cfa0
Mugo-Ndungu <twinnymugo@gmail.com>
2018-12-08 11:43:13 +0300

    Initialize variables to fetch selected option

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,12 @@
 
+var ansOne = $("input[name='ques1']:checked").val();
+var ansTwo = $("input[name='ques2']:checked").val();
+var ansThree = $("input[name='ques3']:checked").val();
+var ansFour = $("input[name='ques4']:checked").val();
+var ansFive = $("input[name='ques5']:checked").val();
+var ansSix = $("input[name='ques6']:checked").val();
+var ansSeven = $("input[name='ques7']:checked").val();
+var ansEight = $("input[name='ques8']:checked").val();
+var ansNine = $("input[name='ques9']:checked").val();
+var ansTen = $("input[name='ques10']:checked").val();
+var correctScore = 0;
